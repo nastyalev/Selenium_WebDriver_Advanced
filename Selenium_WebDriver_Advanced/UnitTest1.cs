9dc537c22843c2cbb54592e11cd370447fa73c40
Nastya Levitova <nastya.levitova99@gmail.com>
2020-05-28 01:32:46 +0300

    lab4

---
+++ Selenium_WebDriver_Advanced/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Selenium_WebDriver_Advanced
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
